bacd163ef44b7be1edd9fb2299ee059c2472fd49
vishnu <zarvis1287@gmail.com>
2020-01-18 11:20:49 +0530

    Nes

--- app.py
+++ app.py
@@ -4,10 +4,33 @@ import json
 from flask_mail import Mail,Message
 import mysql.connector
 
+with open("config.json","r") as c:
+    params=json.load(c)["params"]
+
 app = Flask(__name__)
 
 @app.route('/')
+def start():
+    return render_template('index.html')
+
+@app.route('/home')
 def home():
     return render_template('index.html')
+
+@app.route('/mentors')
+def show_mentors():
+    return render_template('mentors.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html',params=params)
+
+@app.route('/register')
+def register():
+    return render_template('register.html',params=params)
+
+@app.route('/events')
+def events():
+    return render_template('events.html',params=params)
 
 app.run(debug=True)
